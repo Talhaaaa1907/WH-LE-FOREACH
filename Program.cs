dd059c8cb4d0b2461d09e9c274f550f4c946cafc
Talhaaaa1907 <talhafener680@gmail.com>
2021-08-30 22:40:50 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WHİLE_FOREACH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            //1'den başlayarak console'dan girilen sayıya kadar(sayı dahil) ortalama hesaplayıp console'a yazdıran program
+            int sayac = 1;
+            int toplam = 0;
+            Console.WriteLine("Bir sayı giriniz...");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine("Ortalama: " + toplam / sayi);
+
+
+            //a'dan z'ye kadar tüm harfleri console'a yazdır.
+            char karakter = 'a';
+            while (karakter < 'z')
+            {
+                Console.WriteLine(karakter);
+                karakter++;
+            }
+
+
+            //Foreach
+            string[] arabalar = { "BMW", "Mercedes", "Wolksvagen", "Audi" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
